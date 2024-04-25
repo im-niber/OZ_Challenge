9f0c483bfbef238214d4d06c0c8c3a382a16c301
rbwo552 <rbwo552@naver.com>
2024-04-25 22:34:02 +0900

    Flask - Day01

---
+++ Flask/day01/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    users = [
+        {"username": "traveler", "name": "Alex"},
+        {"username": "photographer", "name": "Sam"},
+        {"username": "gourmet", "name": "Chris"}
+    ]
+    # 1. rendering 할 html 파일명 입력
+    # 2. html로 넘겨줄 데이터 입력
+    return render_template('index.html', data=users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
